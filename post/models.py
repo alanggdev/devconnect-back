73bbf131f79c88c438c8d2adc86a4bcc956b7892
Alan Gomez <soyalangg@gmail.com>
2023-06-10 17:12:25 -0600

    added media files in model post

--- post/models.py
+++ post/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class PostModel(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
     description = models.TextField(blank=True)
+    media = models.FileField(upload_to='post/media/', null=True)
     date = models.CharField(max_length=25, null=True)
     likes = models.ManyToManyField(User, blank=True, related_name='likes')
     # comments = models.ForeignKey(CommentModel, on_delete=models.CASCADE, null=True)
